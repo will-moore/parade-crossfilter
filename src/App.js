1169dfde210b036ee968d162b67379eeb20f7057
William Moore <w.moore@dundee.ac.uk>
2019-09-21 07:17:24 +0100

    Add DataTable in main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
 import Drawer from './Drawer.js'
+import DataTable from './table/DataTable';
 
 import {DataContext} from './crossfilter/DataContext';
 
@@ -9,102 +10,39 @@ import './App.css';
 
 function App() {
 
-  const mainStyle = {
-    flex: '1 1 auto',
-    paddingLeft: 15,
-    paddingRight: 15,
-  }
+    const mainStyle = {
+        flex: '1 1 auto',
+        paddingLeft: 15,
+        paddingRight: 15,
+        display: 'flex',
+        flexDirection: 'column',
+        border: 'solid green 2px',
+        width: '200',
+    }
 
-  const [projectsCount, setProjectsCount] = useState(undefined);
-  const [projects, setProjects] = useState([]);
+    return (
+        <div className="App">
+            <DataContext>
+            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <span className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">OMERO.parade</span>
+        </nav>
 
-  useEffect(() => {
-    let projectsUrl = window.OMEROWEB_INDEX + "api/v0/m/projects/";
-    fetch(projectsUrl, {mode: 'cors', credentials: 'include'})
-    .then(rsp => rsp.json())
-    .then(data => {
-      if (data.meta) {
-        let projectCount = data.meta.totalCount;
-        setProjectsCount(projectCount);
-        setProjects(data.data);
-      } else {
-        setProjectsCount(-1);
-      }
-    })
-  }, []);
-
-  return (
-    <div className="App">
-      <DataContext>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <span className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">OMERO.parade</span>
-      </nav>
-
-      <div className="container-fluid">
-        <div className="row" style={{display: 'flex'}}>
-          <Drawer />
-          <main role="main" style={mainStyle}>
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 className="h2">Dashboard</h1>
-              <div className="btn-toolbar mb-2 mb-md-0">
-                <div className="btn-group mr-2">
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
-                </div>
-                <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
-                  <span data-feather="calendar"></span>
-                  This week
-                </button>
-              </div>
+        <div className="container-fluid">
+            <div className="row" style={{display: 'flex', flexWrap: 'nowrap'}}>
+                <Drawer />
+                <main role="main" className="column" style={mainStyle}>
+                    <div style={{background: 'rebeccapurple', height: '50%'}}>
+                        <h2>Plot</h2>
+                    </div>
+                    <div style={{background: 'blue', height: '50%', overflow: 'auto'}}>
+                        <DataTable />
+                    </div>
+                </main>
             </div>
-
-            <p>
-                    {projectsCount === undefined ? "Loading..." : (
-                      `Total: ${projectsCount} projects`
-                    )}
-                </p>
-
-                <Button>Test</Button>
-
-                <ul>
-                  {
-                    projects.map(p => (
-                      <li key={p['@id']}>
-                        {p.Name} (ID: {p['@id']})
-                      </li>
-                    ))
-                  }
-                </ul>
-
-            <h2>Table</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
         </div>
-      </div>
-      </DataContext>
-    </div>
-  );
+        </DataContext>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/table/DataTable.js
@@ -0,0 +1,47 @@
+import React from "react";
+import * as dc from "dc";
+import "dc/dc.css";
+import { CXContext } from "../crossfilter/DataContext";
+
+const tableFunc = (divRef, ndx, columns) => {
+    let colNames = columns.map(c => c.name);
+    const table = dc.dataTable(divRef);
+    const dimension = ndx.dimension(d=> d.Count);
+    table.dimension(dimension)
+    .columns(colNames);
+
+    return table;
+}
+
+const DataTable = props => {
+    /*
+    We render the dc chart using an effect. We want to pass the chart as a prop after the dc call,
+    but there is nothing by default to trigger a re-render and the prop, by default would be undefined.
+    To solve this, we hold a state key and increment it after the effect ran. 
+    By passing the key to the parent div, we get a rerender once the chart is defined. 
+    */
+    const context = React.useContext(CXContext);
+    const [chart, updateChart] = React.useState(null);
+    const ndx = context.ndx;
+    const columns = context.columns;
+    console.log('columns', columns);
+    const table = React.useRef(null);
+    React.useEffect(() => {
+        const newChart = tableFunc(table.current, ndx, columns); // chartfunction takes the ref and does something with it
+        newChart.render();
+
+        updateChart(newChart);
+
+        // Specify how to clean up after this effect:
+        return () => {
+            newChart.dimension().dispose();
+            dc.redrawAll();
+        };
+    }, []);
+
+    return (
+        <table ref={table} className="table table-striped table-sm"></table>
+    );
+};
+
+export default DataTable;
