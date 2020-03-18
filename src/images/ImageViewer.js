85debcffe110deb451706e31b190df31b229ee8c
William Moore <w.moore@dundee.ac.uk>
2020-03-18 23:55:18 +0000

    Load Well images in right panel

--- src/images/ImageViewer.js
+++ src/images/ImageViewer.js
@@ -6,32 +6,51 @@ const ImageViewer = ({rowData}) => {
 
     const [imgData, setImgData] = React.useState(undefined)
     const [roiData, setRoiData] = React.useState(undefined)
+    const [wellData, setWellData] = React.useState(undefined)
 
     React.useEffect(() => {
         setImgData(undefined);
 
         async function loadAsyn() {
             // Get ROI data if we have ROI...
-            if (rowData.ROI) {
+            if (Number.isInteger(rowData.ROI)) {
                 let url = window.OMEROWEB_INDEX + `api/v0/m/rois/${ rowData.ROI }/`;
                 let roiJson = await fetchJson(url);
                 setRoiData(roiJson.data);
             }
             // Load Image Data...
-            let u = window.OMEROWEB_INDEX + `webclient/imgData/${ rowData.Image }/`;
-            let jsonData = await fetchJson(u);
-            setImgData(jsonData);
+            if (Number.isInteger(rowData.Image)) {
+                let u = window.OMEROWEB_INDEX + `webclient/imgData/${ rowData.Image }/`;
+                let jsonData = await fetchJson(u);
+                setImgData(jsonData);
+            // Otherwise load Well Data...
+            } else if (Number.isInteger(rowData.Well)) {
+                let u = window.OMEROWEB_INDEX + `api/v0/m/wells/${ rowData.Well }/`;
+                let jsonData = await fetchJson(u);
+                setWellData(jsonData);
+            }
         }
         loadAsyn();
     }, [rowData]);
 
     return (
         <div>
-            {!imgData ? <div>Loading...</div> :
+            {imgData ?
                 (<div>
                     <p>{imgData.meta.imageName}</p>
                     <ImagePlane imgData={imgData} roiData={roiData} />
-                </div>)
+                </div>) :
+                wellData ? (
+                    <div>
+                        {
+                            wellData.data.WellSamples.map(ws => (
+                                <img src={window.OMEROWEB_INDEX + `webclient/render_image/${ ws.Image['@id'] }/0/0/`}
+                                    style={{maxWidth: 250, margin: 5}}
+                                />
+                            ))
+                        }
+                    </div>
+                ) : <div>Loading...</div>
             }
         </div>
     );
