336677b0193162c5084e15b2fddeee2746f1365d
William Moore <w.moore@dundee.ac.uk>
2021-11-19 16:08:41 +0000

    Version 0.0.2dev

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='parade-crossfilter',
-    version='0.0.1',
+    version='0.0.2dev',
     description="OMERO.web plugin to test crossfilter",
     long_description=long_description,
     long_description_content_type="text/markdown",
