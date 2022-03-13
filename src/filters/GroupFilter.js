3385b5ceaa7cd5ac36d4ae115bc376989bdc24bb
William Moore <w.moore@dundee.ac.uk>
2022-03-13 14:52:21 +0000

    GroupFilter doesn't show barChart if > 12 values in group

--- src/filters/GroupFilter.js
+++ src/filters/GroupFilter.js
@@ -2,40 +2,37 @@ import React from "react";
 import * as dc from "dc";
 import { CXContext } from "../crossfilter/DataContext";
 
-const rowChartFunc = (divRef, ndx, dimName) => {
-    const chart = dc.rowChart(divRef)
-    const dimension = ndx.dimension(function(d) {return d[dimName];});
-    const group = dimension.group()
-    chart.dimension(dimension)
-        .group(group)
-
-    return chart
-}
-
 const GroupFilter = props => {
 
     const context = React.useContext(CXContext);
-    // const [chart, updateChart] = React.useState(null);
-    // const [isFiltered, setFiltered] = React.useState(false);
     const ndx = context.ndx;
     const div = React.useRef(null);
-    const {dimName} = props;
-    React.useEffect(() => {
-        const newChart = rowChartFunc(div.current, ndx, dimName); // chartfunction takes the ref and does something with it
+    const { dimName } = props;
 
-        newChart.render();
+    const dimension = ndx.dimension(function (d) { return d[dimName]; });
+    const group = dimension.group();
+    const size = group.size();
+
+    React.useEffect(() => {
+        if (group.size() > 12) {
+            // Too many items to show in bar chart. Don't create chart.
+            return;
+        }
+        const chart = dc.rowChart(div.current);
+        chart.dimension(dimension).group(group);
+        chart.render();
 
         // Specify how to clean up after this effect:
         return () => {
-            newChart.dimension().dispose();
+            chart.dimension().dispose();
             dc.redrawAll();
         };
-    }, [dimName, ndx]);
-    
+    }, [dimension, group]);
+
     const chartStyles = {
-        width:'100%',
-        height:'auto',
-        boxSizing:'border-box',
+        width: '100%',
+        height: 'auto',
+        boxSizing: 'border-box',
         padding: 0,
         position: 'relative',
     }
@@ -43,7 +40,9 @@ const GroupFilter = props => {
         <div
             ref={div}
             style={chartStyles}
-            >
+        >
+            {/* placeholder text, replaced by chart IF we render it */}
+            <div style={{ color: '#666' }}>Group size: {size}</div>
 
         </div>
     );
