a34faeff78a6f57ac2a5041b348f2e671b96483e
William Moore <w.moore@dundee.ac.uk>
2023-05-03 11:23:47 +0100

    urls.py uses re_path

--- parade_crossfilter/urls.py
+++ parade_crossfilter/urls.py
@@ -15,28 +15,28 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
 
     # index 'home page' of the app
-    url(r'^$', views.index, name='parade_crossfilter_index'),
+    re_path(r'^$', views.index, name='parade_crossfilter_index'),
 
     # get Dataset info for all images in Project
-    url(r'^datasets/(?P<project>[0-9]+)/$', views.datasets,
+    re_path(r'^datasets/(?P<project>[0-9]+)/$', views.datasets,
         name='pc_datasets'),
     
     # Get annotations on images within a project e.g. ?type=map
-    url(r'^annotations/project/(?P<project>[0-9]+)/images/$',
+    re_path(r'^annotations/project/(?P<project>[0-9]+)/images/$',
         views.image_annotations, name='pc_image_annotations'),
-    url(r'^annotations/screen/(?P<screen>[0-9]+)/wells/$',
+    re_path(r'^annotations/screen/(?P<screen>[0-9]+)/wells/$',
         views.well_annotations, name='pc_well_annotations'),
 
-    url(r'^omero_table_as_csv/(?P<file_id>[0-9]+)/$',
+    re_path(r'^omero_table_as_csv/(?P<file_id>[0-9]+)/$',
         views.omero_table_as_csv, name='pc_omero_table_as_csv'),
 
     # POST image as {'data': 'data:image/png;base64,fsfssdflksdf....'}
-    url(r'^save_image/$', views.save_image, name='pc_save_image'),
+    re_path(r'^save_image/$', views.save_image, name='pc_save_image'),
 ]
