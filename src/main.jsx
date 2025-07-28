53f6ef317399b43fb2cc5d3dc6c67878ec81422a
William Moore <w.moore@dundee.ac.uk>
2025-07-28 08:53:28 +0100

    Remove <StrictMode> for development

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// StrictMode causes e.g. <ChooseData /> to render twice in dev mode.
+// https://react.dev/reference/react/StrictMode#fixing-bugs-found-by-double-rendering-in-development
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 console.log("main.jsx...");
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
