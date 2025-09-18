bd699970501d2d013ac4ea285f5ef560e1e8de1f
William Moore <w.moore@dundee.ac.uk>
2025-09-18 10:35:57 +0100

    Use dynamic axes selection for scatter-plot. But Bug on drag-select

--- src/plots.js
+++ src/plots.js
@@ -3,26 +3,35 @@ import * as vg from "@uwdata/vgplot";
 export function scatterPlot(
   table_name,
   selection,
-  xaxis,
-  yaxis,
+  numberColNames,
   width,
   height,
   plotId
 ) {
-  return vg.plot(
-    vg.dot(vg.from(table_name, { filterBy: selection }), {
-      x: xaxis,
-      y: yaxis,
-      tip: true,
-      fill: "steelblue",
-      fillOpacity: 0.8,
-      r: 2,
-    }),
-    vg.intervalXY({ as: selection }),
-    vg.xyDomain(vg.Fixed),
-    vg.width(width),
-    vg.height(height),
-    vg.style({ id: plotId }),
+  // https://github.com/uwdata/mosaic/discussions/374
+  const numberCols = numberColNames.map(d => ({label: d, value: vg.column(d)}));
+  const $x = vg.Param.value(numberCols[0]);
+  const $y = vg.Param.value(numberCols[1]);
+  return vg.vconcat(
+    vg.hconcat(
+      vg.menu({ label: "X-axis", as: $x, options: numberCols }),
+      vg.menu({ label: "Y-axis", as: $y, options: numberCols }),
+    ),
+    vg.plot(
+      vg.dot(vg.from(table_name, { filterBy: selection }), {
+        x: vg.sql`${$x}`,
+        y: vg.sql`${$y}`,
+        tip: true,
+        fill: "steelblue",
+        fillOpacity: 0.8,
+        r: 2,
+      }),
+      vg.intervalXY({ as: selection }),
+      // vg.xyDomain(vg.Fixed),
+      vg.width(width),
+      vg.height(height),
+      vg.style({ id: plotId }),
+    )
   );
 }
 
